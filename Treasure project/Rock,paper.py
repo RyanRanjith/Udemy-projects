0c51c6d0fdceef1cacaf5799a7f57c419c1041e4
Ranjith Gopinath <112666101+ranjithrag1@users.noreply.github.com>
2023-07-30 23:18:50 -0700

    Password generator

--- Treasure project/Rock,paper.py
+++ Treasure project/Rock,paper.py
@@ -1,53 +1,57 @@
 import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
-game_images = [rock, paper, scissors]
-user_choice  =  int(input("Type 0 for rock, 1 for paper 2 for scissors.\n"))
-
-if user_choice>=3 or user_choice < 0:
-    print("invalid")
-else:
-    print(game_images[user_choice])
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
-    computer_choice = random.randint(0, 2)
-    print(f"Computer chose:")
-    print(game_images[computer_choice])
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#  password +=random.choice(letters)
+ 
+
+# for char in range(1, nr_symbols + 1):
+#  password +=random.choice(symbols)
+ 
+
+# for char in range(1, nr_numbers + 1):
+#  password +=random.choice(numbers)
+ 
+# print(password)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
 
-    if user_choice == 0 and computer_choice == 2:
-        print("User wins")
-    elif computer_choice == 0 and user_choice == 2:
-        print("zcomputer wins")
-    elif computer_choice > user_choice:
-        print("computer wins!")
-    elif user_choice>computer_choice:
-        print("user wins")
-    elif computer_choice == user_choice:
-        print("Its a draw")
 
+password_list = []
+
+for char in range(1, nr_letters + 1):
+ password_list.append(random.choice(letters))
+ 
+
+for char in range(1, nr_symbols + 1):
+ password_list +=random.choice(symbols)
+ 
+
+for char in range(1, nr_numbers + 1):
+ password_list +=random.choice(numbers)
+ 
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"your passsword is : {password}")
